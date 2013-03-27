081f357f2e0ecd9c445af3ec5ec393513024a34b
doeswayneexist <doeswayneeexist@gmail.com>
2013-03-27 12:02:01 -0700

    added init js code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+//a form is filled out
+//the user has entered the required data to add an event to the timeline
+
+var addIt = document.getElementById('addIt');
+addIt.onclick = function () {
+	
+	//get the value of the inputs
+
+	//convert to a ts
+
+	//create an event object with the ts and element values
+
+	//run function that adds events to pages
+
+};
+
+
+
+//get the values from the form
+
+//create a new event object to hold those values
+
+//run a function that places all events on the timeline
+
+//place the event on the timeline
+
+//create an object to hold all those event objects
 
